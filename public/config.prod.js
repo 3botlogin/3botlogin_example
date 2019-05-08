266c6367ef5cf825d4f4e145af2e7c665b74a1ef
Jonas Delrue <jonas.delrue@jimber.org>
2019-05-08 15:09:16 +0200

    fixing prod

--- public/config.prod.js
+++ public/config.prod.js
@@ -1,6 +1,6 @@
 export default({
   botForntEnd: 'https://login.threefold.me',
-  botBackend: 'https://login.threefold.me/api',
+  botBackend: 'https://login.threefold.me/',
   redirect_url: `${window.location.protocol}//${window.location.host}`,
   seedPhrase: 'weather smooth little world side palace green armor busy view solution escape' // This is an example of a seedphrase, only used for this example
 })
