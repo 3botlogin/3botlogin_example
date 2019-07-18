553875d72533da7ef11717b14fcb5b43a37a26d4
Alexander Mol <azertyalex@hotmail.com>
2019-07-18 11:38:14 +0200

    config

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 export default({
-  botForntEnd: 'http://localhost:8080',
+  botForntEnd: 'http://localhost:8081',
   botBackend: 'http://localhost:5000',
   redirect_url: `${window.location.protocol}//${window.location.host}`,
   seedPhrase: 'weather smooth little world side palace green armor busy view solution escape' // This is an example of a seedphrase, only used for this example
