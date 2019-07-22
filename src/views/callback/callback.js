958c2a4fd175b5d6c68ec8d908919c8fe449b362
Alexander Mol <azertyalex@hotmail.com>
2019-07-22 15:33:20 +0200

    error callback

--- src/views/callback/callback.js
+++ src/views/callback/callback.js
@@ -1,21 +1,28 @@
 import config from '../../../public/config'
 import cryptoService from '../../services/CryptoService'
 import threebotService from '../../services/threebotService'
+
 export default {
   name: 'callback',
   data () {
     return {
       username: null,
-      verified: false
+      verified: false,
+      error: null
     }
   },
   mounted () {
     var url = new URL(window.location.href)
     // Get all query params
+    this.error = url.searchParams.get('error')
     var username = url.searchParams.get('username')
     var signedhash = url.searchParams.get('signedhash')
     this.username = username
 
+    if(this.error) {
+      console.log("show some error", this.error)
+      return
+    }
     threebotService.getUserData(username).then(async (response) => {
       var user = response.data
       var hash = window.localStorage.getItem('state')
