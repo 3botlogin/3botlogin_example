01f60d6ac891aa1a5c1bef4478259eb60de388ec
jasper.baes <jasper.baes@student.howest.be>
2019-08-26 08:39:43 +0200

    add scopes

--- public/config.js
+++ public/config.js
@@ -1,8 +1,9 @@
 export default({
-  botFrontEnd: 'http://192.168.2.50:8080',
-  botBackend: 'http://192.168.2.50:5000',
+  botFrontEnd: 'http://localhost:8082',
+  botBackend: 'http://localhost:5000',
   // botBackend: 'https://login.staging.jimber.org/',
-  redirect_url: '/callback',
+  redirect_url: `/callback`,
   appId: window.location.host,
   seedPhrase: 'weather smooth little world side palace green armor busy view solution escape' // This is an example of a seedphrase, only used for this example
 })
+
