90c00a3d4312c4b478b0fe8baf20bc5b9e69d615
Mathias <mathias.de.weerdt@gmail.com>
2019-09-09 14:06:07 +0200

    Fixed race cond.

--- src/services/CryptoService.js
+++ src/services/CryptoService.js
@@ -7,6 +7,7 @@ const bip39 = require('bip39')
 export default ({
   validateSignature (message, signature, publicKey) {
     return new Promise(async (resolve, reject) => {
+      await sodium.ready;
       publicKey = decodeBase64(publicKey)
       signature = decodeBase64(signature)
       var result = sodium.crypto_sign_open(signature, publicKey)
@@ -17,6 +18,7 @@ export default ({
   decrypt (message, nonce, privateKey, pubkey) {
     return new Promise(async (resolve, reject) => {
       message = decodeBase64(message)
+      await sodium.ready;
       privateKey = sodium.crypto_sign_ed25519_sk_to_curve25519(decodeBase64(privateKey))
       pubkey = sodium.crypto_sign_ed25519_pk_to_curve25519(decodeBase64(pubkey))
       nonce = decodeBase64(nonce)
@@ -28,6 +30,7 @@ export default ({
   encrypt (message, privateKey, pubkey) {
     return new Promise(async (resolve, reject) => {
       message = new TextEncoder().encode(message)
+      await sodium.ready;
       privateKey = sodium.crypto_sign_ed25519_sk_to_curve25519(decodeBase64(privateKey))
       pubkey = sodium.crypto_sign_ed25519_pk_to_curve25519(decodeBase64(pubkey))
 
@@ -41,6 +44,7 @@ export default ({
   },
   generateKeys (phrase) {
     return new Promise(async (resolve, reject) => {
+      await sodium.ready;
       if (!phrase) phrase = bip39.entropyToMnemonic(sodium.randombytes_buf(sodium.crypto_box_SEEDBYTES / 2))
       var ken = new TextEncoder().encode(bip39.mnemonicToEntropy(phrase))
       var keys = sodium.crypto_sign_seed_keypair(ken)
